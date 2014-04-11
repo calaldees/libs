58ac5edafce545ae5b3d7eaeecf8eeadec4260de
Allan Callaghan <calaldees@hotmail.com>
2014-04-11 09:37:34 +0100

    Competed the simple encrypt,decrypt functions

--- python3/lib/crypt.py
+++ python3/lib/crypt.py
@@ -16,23 +16,40 @@ PADDING = '|'
 
 DEFAULT_METHOD = AES
 
+def _pad(data, block_size=BLOCK_SIZE, padding=PADDING):
+    if isinstance(data, bytes):  # ensure we can pad bytes or strings by making padding match the data type
+        padding = padding.encode('utf-8')
+    return data + (block_size - len(data) % block_size) * padding
+
 def encrypt(data, secret=None, method=DEFAULT_METHOD, block_size=BLOCK_SIZE, padding=PADDING):
     """
     Encrypt a python datastructure with a key as a base 64 string
     >>> data = {'a': [1, 2, 3]}
     >>> encrypt(data, 'password')
-    '273fbw38y3wap'
+    b'7HXC5jodYm6SSS7gFSGyM/Dqj6i16sgBbrMt6fenJ6c='
     """
-    pad = lambda s: s + (block_size - len(s) % block_size) * padding
     if not secret:
         secret = os.urandom(block_size)
-    return base64.b64encode(method.new(secret).encrypt(pad(json.dumps(data))))
+    secret = _pad(secret, block_size, padding)
+    return base64.b64encode(
+        method.new(secret).encrypt(
+            _pad(
+                json.dumps(data),
+                block_size, padding
+            ).encode('utf-8')
+        )
+    )
 
-def decrypt(data, secret, method=DEFAULT_METHOD, padding=PADDING):
+def decrypt(data, secret, method=DEFAULT_METHOD, block_size=BLOCK_SIZE, padding=PADDING):
     """
     Decrypt an encrypted base 64 string into a python datastructure using a key
-    >>> data = '273fbw38y3wap'
+    >>> data = b'7HXC5jodYm6SSS7gFSGyM/Dqj6i16sgBbrMt6fenJ6c='
     >>> decrypt(data, 'password')
     {'a': [1, 2, 3]}
     """
-    return json.loads(method.new(secret).decrypt(base64.b64decode(data)).rstrip(padding))
+    secret = _pad(secret, block_size, padding)
+    return json.loads(
+        method.new(secret).decrypt(
+            base64.b64decode(data)
+        ).decode('utf-8').rstrip(padding)
+    )
