27514e1aeb20ce7b6cd5c7527ac2f9f64cac576e
Allan Callaghan <calaldees@hotmail.com>
2014-04-14 07:46:14 +0100

    Refectored Cryptor into a class

--- python3/lib/crypt.py
+++ python3/lib/crypt.py
@@ -3,53 +3,63 @@ import base64
 import os
 import json
 
-# Inspied by
-#  http://sentdex.com/sentiment-analysisbig-data-and-python-tutorials-algorithmic-trading/encryption-and-decryption-in-python-code-example-with-explanation/
 
-# the block size for cipher obj, can be 16 24 or 32. 16 matches 128 bit.
-#32 bytes = 256 bits
-#16 = 128 bits
-BLOCK_SIZE = 16
-
-# the character used for padding - used to ensure that your value is always a multiple of BLOCK_SIZE
-PADDING = '|'
-
-DEFAULT_METHOD = AES
-
-def _pad(data, block_size=BLOCK_SIZE, padding=PADDING):
-    if isinstance(data, bytes):  # ensure we can pad bytes or strings by making padding match the data type
-        padding = padding.encode('utf-8')
-    return data + (block_size - len(data) % block_size) * padding
-
-def encrypt(data, secret=None, method=DEFAULT_METHOD, block_size=BLOCK_SIZE, padding=PADDING):
+class Cryptor(object):
     """
-    Encrypt a python datastructure with a key as a base 64 string
-    >>> data = {'a': [1, 2, 3]}
-    >>> encrypt(data, 'password')
-    b'7HXC5jodYm6SSS7gFSGyM/Dqj6i16sgBbrMt6fenJ6c='
+    Encrypt/Decrypt Python data structures to/from base64 strings
+    
+    Inspied by
+        http://sentdex.com/sentiment-analysisbig-data-and-python-tutorials-algorithmic-trading/encryption-and-decryption-in-python-code-example-with-explanation/
+    The block size for cipher obj, can be 16 24 or 32.
+        32 bytes = 256 bits
+        16 bytes = 128 bits
+    The character used for padding - used to ensure that your value is always a multiple of BLOCK_SIZE
+    
+    Todo:
+        Add custom json serializer support
     """
-    if not secret:
-        secret = os.urandom(block_size)
-    secret = _pad(secret, block_size, padding)
-    return base64.b64encode(
-        method.new(secret).encrypt(
-            _pad(
-                json.dumps(data),
-                block_size, padding
-            ).encode('utf-8')
+    DEFAULT_METHOD = AES
+    BLOCK_SIZE = 16
+    PADDING = '|'
+    
+    def __init__(self, secret=None, method=DEFAULT_METHOD, block_size=BLOCK_SIZE, padding=PADDING):
+        self.method = method
+        self.block_size = block_size
+        self.padding = padding
+        if not secret:
+            secret = os.urandom(block_size)
+        self.secret = self._pad(secret)
+
+    def _pad(self, data):
+        padding = self.padding
+        if isinstance(data, bytes):  # ensure we can pad bytes or strings by making padding match the data type
+            padding = self.padding.encode('utf-8')
+        return data + (self.block_size - len(data) % self.block_size) * padding
+    
+    def encrypt(self, data):
+        """
+        Encrypt a python datastructure with a key as a base 64 string
+        >>> data = {'a': [1, 2, 3]}
+        >>> Cryptor(secret='password').encrypt(data)
+        b'7HXC5jodYm6SSS7gFSGyM/Dqj6i16sgBbrMt6fenJ6c='
+        """
+        return base64.b64encode(
+            self.method.new(self.secret).encrypt(
+                self._pad(
+                    json.dumps(data)
+                ).encode('utf-8')
+            )
         )
-    )
-
-def decrypt(data, secret, method=DEFAULT_METHOD, block_size=BLOCK_SIZE, padding=PADDING):
-    """
-    Decrypt an encrypted base 64 string into a python datastructure using a key
-    >>> data = b'7HXC5jodYm6SSS7gFSGyM/Dqj6i16sgBbrMt6fenJ6c='
-    >>> decrypt(data, 'password')
-    {'a': [1, 2, 3]}
-    """
-    secret = _pad(secret, block_size, padding)
-    return json.loads(
-        method.new(secret).decrypt(
-            base64.b64decode(data)
-        ).decode('utf-8').rstrip(padding)
-    )
+    
+    def decrypt(self, data):
+        """
+        Decrypt an encrypted base 64 string into a python datastructure using a key
+        >>> data = b'7HXC5jodYm6SSS7gFSGyM/Dqj6i16sgBbrMt6fenJ6c='
+        >>> Cryptor(secret='password').decrypt(data)
+        {'a': [1, 2, 3]}
+        """
+        return json.loads(
+            self.method.new(self.secret).decrypt(
+                base64.b64decode(data)
+            ).decode('utf-8').rstrip(self.padding)
+        )
