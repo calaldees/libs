619a156aae96e100f47ffd5497cd93f2fa8f0105
Allan Callaghan <calaldees@gmail.com>
2014-12-24 20:12:34 +0000

    Corrected timestamp output for events (use iso)

--- python3/lib/log.py
+++ python3/lib/log.py
@@ -14,6 +14,6 @@ def log_event(request, **kwargs):
         'event': data.get('event') or request.matched_route.name,
         'user_id': request.session.get('id'),
         'user_ip': request.environ.get('REMOTE_ADDR'),
-        'timestamp': int(now().timestamp()),
+        'timestamp': now(),
     })
     event_log.info(json_string(data))
