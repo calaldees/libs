405ef1b06682616b0a453da38a18e8b6598d97c6
Allan Callaghan <calaldees@gmail.com>
2014-12-26 19:06:50 +0000

    Modifyed log_event fieldnames

--- python3/lib/log.py
+++ python3/lib/log.py
@@ -12,8 +12,8 @@ def log_event(request, **kwargs):
     data = kwargs
     data.update({
         'event': data.get('event') or request.matched_route.name,
-        'user_id': request.session.get('id'),
-        'user_ip': request.environ.get('REMOTE_ADDR'),
+        'session_id': request.session.get('id'),
+        'ip': request.environ.get('REMOTE_ADDR'),
         'timestamp': now(),
     })
     event_log.info(json_string(data))
