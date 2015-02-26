7c6a6b47f08939e86cbb81be5d3a75abc8a23d19
Allan Callaghan <calaldees@gmail.com>
2015-02-26 07:58:44 +0000

    Always strip the newline chars for '-> listfile'

--- python3/lib/misc.py
+++ python3/lib/misc.py
@@ -141,7 +141,7 @@ def read_file_list(filename):
     data = []
     with open(filename, 'r') as source:
         for line in source:
-            data.append(line)
+            data.append(line.strip())
     return data
 
 
