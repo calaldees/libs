6c524eccd8bd921c270daaf267439639b66d280b
Allan Callaghan <calaldees@gmail.com>
2015-05-13 08:31:59 +0100

    Updated the midi inputto be more reuseable

--- python3/lib/midi/pygame_midi_input.py
+++ python3/lib/midi/pygame_midi_input.py
@@ -14,7 +14,7 @@ class MidiInput(object):
     def __init__(self, midi_input_name=None):
         self.midi_input_name = midi_input_name
 
-    def open(self):
+    def init_pygame(self):
         pygame.init()
         pygame.display.set_caption(__name__)
         pygame.event.set_blocked(pygame.MOUSEMOTION)
@@ -23,31 +23,53 @@ class MidiInput(object):
         self.event_get = pygame.fastevent.get
         self.event_post = pygame.fastevent.post
 
+        self.init_midi_input()
+        #pygame.display.set_mode((1, 1))
+        #self.run()
+        self.running = False
+
+    def init_midi_input(self):
         pygame.midi.init()
         self.midi_input = PygameMidiDeviceHelper.open_device(self.midi_input_name, 'input')
 
-        #pygame.display.set_mode((1, 1))
-        self.run()
-
-    def _process_events(self):
-        events = self.event_get()
+    def process_events(self, events=None):
+        """
+        Process a pygame eventlist
+        if no eventlist is passed it will get the current eventlist
+        This can be used when inside another game loop
+        """
+        if events == None:
+            self._poll()
+            events = self.event_get()
         for event in events:
-            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            # Escape events (only used when inside the self.run() method
+            if self.running and (event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE)):
                 self.running = False
+            # Midi events
             if event.type in [pygame.midi.MIDIIN]:
                 self.midi_event(midi_status(event.status), event.data1, event.data2, event.data3)
-        if self.midi_input.poll():
-            for midi_event in pygame.midi.midis2events(self.midi_input.read(10), self.midi_input.device_id):
-                self.event_post(midi_event)
 
     def run(self):
+        """
+        Run/Start a standalone loop to read from this input
+        This is useful for test programs or simple standalone applications that don't require a game loop
+        """
         try:
             self.running = True
             while self.running:
-                self._process_events()
+                self.process_events()
+                if self.midi_input.poll():
+                    self._poll()
         except KeyboardInterrupt:
             pass
         self.close()
+
+    def _poll(self):
+        """
+        Used inside an unrestricted loop to block for input
+        """
+        for midi_event in pygame.midi.midis2events(self.midi_input.read(10), self.midi_input.device_id):
+            self.event_post(midi_event)
 
     def close(self):
         del self.midi_input
