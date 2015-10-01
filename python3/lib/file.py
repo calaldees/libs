3c27a009989674d5cc37ea9f14de8a066a4614e0
Allan Callaghan <calaldees@gmail.com>
2015-10-01 08:40:56 +0100

    Adding scaning to in memory files

--- python3/lib/file.py
+++ python3/lib/file.py
@@ -6,7 +6,7 @@ from .misc import file_scan, defaultdict_recursive
 class FolderStructure(object):
 
     @staticmethod
-    def factory(path):
+    def factory(path, **kwargs):
         files = defaultdict_recursive()
 
         def set_leaf(f):
@@ -14,7 +14,7 @@ class FolderStructure(object):
             for path_segment in os.path.split(f.folder):
                 files_item = files_item[path_segment]
             files_item[f.file] = f
-        for f in file_scan(path, stats=True):
+        for f in file_scan(path, stats=True, **kwargs):
             set_leaf(f)
 
         def insert_parent_reference(files_item, parent=None):
@@ -29,10 +29,25 @@ class FolderStructure(object):
         self.files_item = files_item if files_item else {}
 
     def get_folders(self):
-        return ()
+        for key, value in self.files_item.items():
+            if isinstance(value, FolderStructure):
+                yield value
 
     def get_files(self):
-        return ()
+        for key, value in self.files_item.items():
+            if not isinstance(value, FolderStructure):
+                yield value
 
     def get_parent(self):
         return FolderStructure(self.files_item.get('..'))
+
+    def scan(self, file_regex=None, ignore_regex=None):
+        """
+        recusivly look trhough files and yeild files in turn
+        """
+        for file_item in self.files_item.values():
+            if (isinstance(file_item, FolderStructure)):
+                for f in file_item.scan(file_regex, ignore_regex):
+                    yield f
+            elif file_regex.search(file_item.file) and not ignore_regex.search(file_item.file):
+                yield f
