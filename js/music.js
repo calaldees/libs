0f549129721abcc2f0b65b20ffd533dbc3255adf
Allan Callaghan <calaldees@gmail.com>
2016-02-19 19:08:39 +0000

    Started porting python music tools to js

---
+++ js/music.js
@@ -0,0 +1,70 @@
+var music = {};
+
+(function(external){
+
+
+	// Human readable input/output -------------------------------------------------
+	
+	var LOOKUP_NOTE_STR = {
+		0: 'C',
+		1: 'C#',
+		2: 'D',
+		3: 'D#',
+		4: 'E',
+		5: 'F',
+		6: 'F#',
+		7: 'G',
+		8: 'G#',
+		9: 'A',
+		10: 'A#',
+		11: 'B',
+	}
+	var LOOKUP_STR_NOTE = _.invert(LOOKUP_NOTE_STR);
+	var NUM_NOTES_IN_OCTAVE = _.keys(LOOKUP_NOTE_STR).length;
+	var OFFSET_FROM_C0 = NUM_NOTES_IN_OCTAVE * 2;
+
+	function note_to_text(note) {
+		//>>> note_to_text(0)
+		//'C-2'
+		//>>> note_to_text(24)
+		//'C0'
+		//>>> note_to_text(60)
+		//'C3'
+		//>>> note_to_text(61)
+		//'C#3'
+		return "" + LOOKUP_NOTE_STR[note % NUM_NOTES_IN_OCTAVE] +""+ Math.floor((note - OFFSET_FROM_C0)/NUM_NOTES_IN_OCTAVE);
+	}
+
+	function parse_note(item) {
+		/*
+		>>> parse_note('C-2')
+		0
+		>>> parse_note('C0')
+		24
+		>>> parse_note('C3')
+		60
+		>>> parse_note('C#3')
+		61
+		>>> parse_note('60')
+		60
+		>>> parse_note(60)
+		60
+		*/
+		console.log(item);
+		if (Number(item) != Number.NaN) {
+			return Number(item);
+		}
+		console.log(item);
+		var regex_match = item.toUpperCase().match(/([ABCDEFG]#?)(-?\d{1,2})/);
+		console.log(regex_match);
+		if (!regex_match) {
+			console.warn("Unable to parse note", item);
+		}
+		var note_str = regex_match[1];
+		var octave = Number(regex_match[2]);
+		console.log(note_str, octave);
+		console.log(LOOKUP_STR_NOTE[note_str], (octave * NUM_NOTES_IN_OCTAVE), OFFSET_FROM_C0);
+		return LOOKUP_STR_NOTE[note_str] + (octave * NUM_NOTES_IN_OCTAVE) + OFFSET_FROM_C0;
+	}
+
+}(utils));
