6cb5fccd466333a4d5683d289409bbda96ae0bd0
Allan Callaghan <calaldees@gmail.com>
2016-03-18 19:03:01 +0000

    Fix for folders starting with '/'

--- python3/lib/file.py
+++ python3/lib/file.py
@@ -9,7 +9,7 @@ class FolderStructure(object):
 
         def set_leaf(f):
             files_item = files
-            for path_segment in f.folder.split('/'):
+            for path_segment in filter(None, f.folder.split('/')):
                 files_item = files_item[path_segment]
             files_item[f.file] = f
         for f in fast_scan(path, **kwargs):
@@ -24,6 +24,14 @@ class FolderStructure(object):
         for key, value in self._files_item.items():
             if isinstance(value, dict):
                 self._files_item[key] = FolderStructure(value, parent=self, name=key)
+
+    def __str__(self):
+        return 'parent={parent} name={name}\n{folders}\n{files}'.format(
+            parent=self.parent.name if self.parent else '',
+            name=self.name,
+            folders='\n'.join('/{0}'.format(f.name) for f in self.folders),
+            files='\n'.join('{0}'.format(f.file) for f in self.files),
+        )
 
     @property
     def folders(self):
@@ -46,7 +54,7 @@ class FolderStructure(object):
         return self._name
 
     def get(self, path):
-        if isinstance(path, str):
+        if path and isinstance(path, str):
             path = list(path.split('/'))
         if path:
             try:
