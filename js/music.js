18927089c5ad06ed7905d25e9d1dde69ee989b46
Allan Callaghan <calaldees@gmail.com>
2016-04-15 20:42:03 +0100

    Implemented midi_status fuctions from music.py in music.js

--- js/music.js
+++ js/music.js
@@ -62,4 +62,29 @@ var music = {};
 		return LOOKUP_STR_NOTE[note_str] + (octave * NUM_NOTES_IN_OCTAVE) + OFFSET_FROM_C0;
 	}
 
-}(utils));
+	var MIDI_STATUS_LOOKUP = {
+		0x8: 'note_off',
+		0x9: 'note_on',
+		0xA: 'polyphonic_aftertouch',
+		0xB: 'control_change',
+		0xC: 'program_change',
+		0xD: 'channel_aftertouch',
+		0xE: 'pitch_wheel',
+	};
+	
+	function midi_status(status_byte) {
+		var status_code = Math.floor(status_byte/16);
+		return {
+			code: status_code,
+			name:MIDI_STATUS_LOOKUP[status_code],
+			channel: status_byte % 16,
+		};
+	}
+	
+	_.extendOwn(external, {
+		'midi_status': midi_status,
+		'note_to_text': note_to_text,
+		'parse_note': parse_note,
+	});
+
+}(music));
