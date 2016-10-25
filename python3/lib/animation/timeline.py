c1fb1afca32ddc02cf0b7fc3d70ea40ff79d7008
Allan Callaghan <calaldees@gmail.com>
2016-10-25 08:08:48 +0100

    Fleshed out operators for animation

--- python3/lib/animation/timeline.py
+++ python3/lib/animation/timeline.py
@@ -12,7 +12,7 @@ class Timeline(object):
     """
     Inspired by
       * GSAP (GreenSock Animation Platform) - http://greensock.com/
-      * Kia - www.
+      * Kivy - https://kivy.org/docs/api-kivy.animation.html
     A generalised animation framework for tweeing python attributes.
     """
     AnimationItem = namedtuple('AnimationItem', ('timestamp', 'element', 'duration', 'values'))
@@ -20,12 +20,12 @@ class Timeline(object):
     def __init__(self, delay=0, repeat=0, repeatDelay=0, onUpdate=None, onRepeat=None, onComplete=None):
         self._animation_items = []
         self._label_timestamps = {}
-        self._head_timestamp = 0
+        self.duration = 0
 
     # Properties ---------------------------------------------------------------
 
     @property
-    def duration(self):
+    def _duration(self):
         return reduce(
             lambda accumulator, item: max(accumulator, item.timestamp + item.duration),
             self._animation_items, 0
@@ -38,8 +38,8 @@ class Timeline(object):
         if not hasattr(elements, '__iter__'):
             elements = (elements, )
 
-        # Lookup/Calculate label timestmpa
-        timestamp = self._head_timestamp
+        # Lookup/Calculate label timestamp
+        timestamp = self.duration
         if isinstance(label, Number):
             timestamp += label
         else:
@@ -66,7 +66,7 @@ class Timeline(object):
     # Decorators ---------------------------------------------------------------
 
     def _invalidate_timeline_cache(self):
-        log.debug('TODO: cache cleared')
+        self.duration = self._duration
     def invalidate_timeline_cache(original_function=None):
         """
         Decorator to place on methods that modify the timeline state
@@ -105,37 +105,63 @@ class Timeline(object):
             setattr(t, field, copy(getattr(self, field)))
         return t
 
+    def _add_(timeline1, timeline2):
+        timeline1._animation_items += [
+            self.AnimationItem(timestamp=timeline1.duration + i.timestamp, element=i.element, duration=i.duration, values=i.values)
+            for i in timeline2._animation_items
+        ]
+        timeline1._label_timestamps.update({
+            label: timeline1.duration + timestamp
+            for label, timestamp in timeline2._label_timestamps.items()
+        })
+        timeline1._invalidate_timeline_cache()
+
+    def __add__(timeline1, timeline2):
+        t = copy(timeline1)
+        t._and_(timeline2)
+        return t
+
     def __iadd__(self, other):
-        pass
-
-    def __add__(self, other):
-        pass
-
-    def __and__(self, other):
-        pass
-
-    def __iand__(self, other):
-        pass
+        self._add_(other)
 
     def __concat__(self, other):
         pass
 
+    def _and_(timeline1, timeline2):
+        timeline1._animation_items += timeline2._animation_items
+        timeline1._label_timestamps.update(timeline2._label_timestamps)
+        timeline1._invalidate_timeline_cache()
+
+    def __and__(timeline1, timeline2):
+        t = copy(timeline1)
+        t._and_(timeline2)
+        return t
+
+    def __iand__(self, other):
+        self._and_(other)
+
+    def _reverse_(timeline):
+        timeline._animation_items = [
+            self.AnimationItem(timestamp=timeline.duration - i.timestamp - i.duration, element=i.element, duration=i.duration, values=i.values)
+            for i in reversed(timeline._animation_items)
+        ]
+
     def __reversed__(self):
-        pass
+        self._reverse_()
 
     def __invert__(self):
-        pass
+        self._reverse_()
 
-    def _mul_(self, repeats):
+    def _mul_(timeline, repeats):
         assert isinstance(repeats, int)
         return [
             self.AnimationItem(timestamp=i.timestamp * r, element=i.element, duration=i.duration, values=i.values)
-            for i in self._animation_items
+            for i in timeline._animation_items
             for r in repeats
         ]
 
-    def __mul__(self, repeats):
-        t = copy(self)
+    def __mul__(timeline, repeats):
+        t = copy(timeline)
         t._animation_items = self._mul_(repeats)
         return t
 
