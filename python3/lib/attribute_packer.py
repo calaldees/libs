f9b2c69fca93cf349cf02909093e7e48fe910a4e
Allan Callaghan <calaldees@gmail.com>
2016-11-01 18:35:52 +0000

    Added doctests to attribute_packer Added pack_size to attribute_packer

--- python3/lib/attribute_packer.py
+++ python3/lib/attribute_packer.py
@@ -12,26 +12,50 @@ class AttributePackerMixin(object):
             'B',
         ),
         'onebyte': AttributeEncoder(
-            lambda value: value * 255,
+            lambda value: int(value * 255),
             lambda value: value / 255,
             'B',
         ),
     }
 
-    def __init__(self, attributes):
-        self.attributes = attributes
-
-    def pack(self, buffer=None, offset=None):
-        """
+    def __init__(self, attributes, assert_attributes_exist=True):
+        r"""
         >>> class tt(AttributePackerMixin):
         ...     def __init__(self):
+        ...         self.a = 0
+        ...         self.b = 0
         ...         AttributePackerMixin.__init__(self, (
         ...             AttributePackerMixin.Attribute('a', 'byte'),
         ...             AttributePackerMixin.Attribute('b', 'onebyte'),
         ...         ))
-        ...         self.a = 10
-        ...         self.b = 0.5
         >>> obj = tt()
+        >>> obj.pack_size
+        2
+
+        """
+        if assert_attributes_exist:
+            for attribute in attributes:
+                assert hasattr(self, attribute.name), """object '{}' should have the required attribute '{}'""".format(self, attribute.name)
+        self.attributes = attributes
+        self.pack_size = sum(struct.calcsize(self.AttributeEncoders[attribute.type].fmt) for attribute in self.attributes)
+
+    def pack(self, buffer, offset):
+        r"""
+        >>> class tt(AttributePackerMixin):
+        ...     def __init__(self):
+        ...         self.a = 16
+        ...         self.b = 0.5
+        ...         AttributePackerMixin.__init__(self, (
+        ...             AttributePackerMixin.Attribute('a', 'byte'),
+        ...             AttributePackerMixin.Attribute('b', 'onebyte'),
+        ...         ))
+        >>> obj = tt()
+        >>> buffer = bytearray(b'\x01\x02\x03\x04')
+        >>> obj.pack(buffer, 1)
+        3
+        >>> buffer
+        bytearray(b'\x01\x10\x7f\x04')
+
         """
         for value, encoder in (
                 (getattr(self, attribute_name), self.AttributeEncoders[attribute_type])
@@ -41,7 +65,25 @@ class AttributePackerMixin(object):
             offset += struct.calcsize(encoder.fmt)
         return offset
 
-    def unpack(self, buffer=None, offset=None):
+    def unpack(self, buffer, offset):
+        r"""
+        >>> class tt(AttributePackerMixin):
+        ...     def __init__(self):
+        ...         self.a = 0
+        ...         self.b = 0
+        ...         AttributePackerMixin.__init__(self, (
+        ...             AttributePackerMixin.Attribute('a', 'byte'),
+        ...             AttributePackerMixin.Attribute('b', 'onebyte'),
+        ...         ))
+        >>> obj = tt()
+        >>> buffer = bytearray(b'\x01\xff\x7f\x04')
+        >>> obj.unpack(buffer, 1)
+        3
+        >>> obj.a
+        255
+        >>> assert (obj.b - 0.5) < 0.01
+
+        """
         for attribute_name, encoder in (
                 (attribute_name, self.AttributeEncoders[attribute_type])
                 for attribute_name, attribute_type in self.attributes
