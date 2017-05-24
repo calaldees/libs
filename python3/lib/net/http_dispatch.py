49e5ac2ea608c7ee83a86abe06b4fd5f21374682
Allan Callaghan <calaldees@gmail.com>
2017-05-24 19:15:29 +0100

    Jibbled http_dispatch to gracefully handle keyboard exeptions. Error 500 on exceptions with plain text response

--- python3/lib/net/http_dispatch.py
+++ python3/lib/net/http_dispatch.py
@@ -6,7 +6,7 @@ import logging
 log = logging.getLogger(__name__)
 
 
-def http_dispatch(func_dispatch, request_regexs=(), port=23487):
+def http_dispatch(func_dispatch, port=23487):
     """
     A super simple single thread tiny http framework
 
@@ -20,54 +20,64 @@ def http_dispatch(func_dispatch, request_regexs=(), port=23487):
      - https://docs.python.org/3/howto/sockets.html
      - https://ruturajv.wordpress.com/2005/12/27/conditional-get-request/
     """
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
-        soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        soc.bind(('', port))
-        soc.listen(1)
-        while True:
-            log.debug(f'HTTP Dispatch: Waiting on {port}')
-            connection, ip_address = soc.accept()
-            log.debug(f'Client Connected: {ip_address}')
-            # TODO: Thread the handling of each request?
-            with connection:
-                data = connection.recv(1024)  # TODO: Loop over recv? We may have larger requests
-                if not data:
-                    break
 
-                request = data.decode('utf8')  # TODO: Separate out head and body and decode separately (to allow binary bodys if required)
-                request_dict = dict(
-                    ((match.group(1), match.group(2)) for match in re.finditer(r'(.*?)\: (.*)', request)),
-                    **re.match(r'(?P<method>.*?) (?P<path>.*) HTTP/1', request).groupdict(),
-                    **{
-                        'ip': ip_address
-                    }
-                )
+    def _handle_request(connection, data):
+        request = data.decode('utf8')  # TODO: Separate out head and body and decode separately (to allow binary bodys if required)
+        request_dict = dict(
+            ((match.group(1), match.group(2)) for match in re.finditer(r'(.*?)\: (.*)', request)),
+            **re.match(r'(?P<method>.*?) (?P<path>.*) HTTP/1', request).groupdict(),
+        )
 
-                # Create stub HTTP response headers
-                response_dict = {
-                    'Server': 'http_dispatch 0.0.0',
-                    'Content-Length': None,
-                    'Content-Type': 'text/plain',
-                    'Connection': 'Closed',
-                    '_status': '200 OK',
-                    '_body': b'',
-                }
+        # Create stub HTTP response headers
+        response_dict = {
+            'Server': 'http_dispatch 0.0.0',
+            'Content-Length': None,
+            'Content-Type': 'text/plain',
+            'Connection': 'Closed',
+            '_status': '200 OK',
+            '_body': b'',
+        }
 
-                # Perform dispatch
-                response_dict = func_dispatch(request_dict, response_dict)
+        # Perform dispatch
+        try:
+            response_dict = func_dispatch(request_dict, response_dict)
+        except Exception as ex:
+            response_dict['_status'] = '500 Internal Server Error'
+            response_dict['_body'] = str(ex).encode('utf8')  # TODO: get full traceback as string and return that
 
-                if not response_dict['Content-Length']:
-                    response_dict['Content-Length'] = len(response_dict['_body'])
+        if not response_dict['Content-Length']:
+            response_dict['Content-Length'] = len(response_dict['_body'])
 
-                # Construct Response
-                connection.sendall('\n'.join(chain(
-                    ('HTTP/1.1 {_status}', ),
-                    tuple(f'{k}: {v}' for k, v in response_dict.items() if not k.startswith('_')),
-                    ('\n', ),
-                )).encode('utf8'))
-                # Send body (if required)
-                if request_dict['method'] != 'HEAD':
-                    connection.sendall(response_dict['_body'])
+        # Construct Response
+        connection.sendall('\n'.join(chain(
+            ('HTTP/1.1 {_status}'.format(**response_dict), ),
+            tuple(f'{k}: {v}' for k, v in response_dict.items() if not k.startswith('_')),
+            ('\n', ),
+        )).encode('utf8'))
+        # Send body (if required)
+        if request_dict['method'] != 'HEAD':
+            connection.sendall(response_dict['_body'])
+
+    def _serve():
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
+            soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            soc.bind(('', port))
+            soc.listen(1)
+            log.info(f'Serving on {port}')
+            try:
+                while True:
+                    log.debug(f'Accepting next connection on {port}')
+                    connection, ip_address = soc.accept()
+                    log.debug(f'Client Connected: {ip_address}')
+                    # TODO: Thread the handling of each request?
+                    with connection:
+                        data = connection.recv(1024)  # TODO: Loop over recv? We may have larger requests
+                        if data:
+                            _handle_request(connection, data)
+            except KeyboardInterrupt:
+                pass
+
+    _serve()
 
 
 if __name__ == "__main__":
