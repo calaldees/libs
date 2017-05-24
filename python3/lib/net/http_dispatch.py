8c43b5bd651e9463cab870b3fbe02937132b4001
Allan Callaghan <calaldees@gmail.com>
2017-05-24 07:36:29 +0100

    Refactored request_dict aquisition to be more flexible Removed unneeded layer of indent Added printing of request to example

--- python3/lib/net/http_dispatch.py
+++ python3/lib/net/http_dispatch.py
@@ -1,16 +1,9 @@
 import socket
 import re
-from functools import reduce
 from itertools import chain
 
 import logging
 log = logging.getLogger(__name__)
-
-
-_DEFAULT_REQUEST_REGEXS = (
-    r'(?P<method>.*?) (?P<path>.*) HTTP/1',
-    r'If-None-Match: "(?P<etag>.*?)"',
-)
 
 
 def http_dispatch(func_dispatch, request_regexs=(), port=23487):
@@ -36,54 +29,51 @@ def http_dispatch(func_dispatch, request_regexs=(), port=23487):
             connection, ip_address = soc.accept()
             log.debug(f'Client Connected: {ip_address}')
             with connection:
-                while True:
-                    data = connection.recv(1024)
-                    if not data:
-                        break
-                    request = data.decode('utf8')
-
-                    # Parse HTTP Request with regex extractors
-                    def reducer(accumulator, regex_string):
-                        try:
-                            accumulator.update(re.search(regex_string, request).groupdict())
-                        except AttributeError:
-                            pass
-                        return accumulator
-                    request_dict = reduce(reducer, chain(_DEFAULT_REQUEST_REGEXS, request_regexs), {'ip': ip_address})
-
-                    # Create stub HTTP response headers
-                    response_dict = {
-                        'Server': 'http_dispatch 0.0.0',
-                        'Content-Length': None,
-                        'Content-Type': 'text/plain',
-                        'Connection': 'Closed',
-                        '_status': '200 OK',
-                        '_body': b'',
-                    }
-
-                    # Perform dispatch
-                    response_dict = func_dispatch(request_dict, response_dict)
-
-                    if not response_dict['Content-Length']:
-                        response_dict['Content-Length'] = len(response_dict['_body'])
-
-                    # Construct Response
-                    connection.sendall('\n'.join(chain(
-                        ('HTTP/1.1 {_status}', ),
-                        tuple(f'{k}: {v}' for k, v in response_dict.items() if not k.startswith('_')),
-                        ('\n', ),
-                    )).encode('utf8'))
-                    # Send body (if required)
-                    if request_dict['method'] != 'HEAD':
-                        connection.sendall(response_dict['_body'])
-
+                data = connection.recv(1024)
+                if not data:
                     break
+
+                request = data.decode('utf8')
+                request_dict = dict(
+                    ((match.group(1), match.group(2)) for match in re.finditer(r'(.*?)\: (.*)', request)),
+                    **re.match(r'(?P<method>.*?) (?P<path>.*) HTTP/1', request).groupdict(),
+                    **{
+                        'ip': ip_address
+                    }
+                )
+
+                # Create stub HTTP response headers
+                response_dict = {
+                    'Server': 'http_dispatch 0.0.0',
+                    'Content-Length': None,
+                    'Content-Type': 'text/plain',
+                    'Connection': 'Closed',
+                    '_status': '200 OK',
+                    '_body': b'',
+                }
+
+                # Perform dispatch
+                response_dict = func_dispatch(request_dict, response_dict)
+
+                if not response_dict['Content-Length']:
+                    response_dict['Content-Length'] = len(response_dict['_body'])
+
+                # Construct Response
+                connection.sendall('\n'.join(chain(
+                    ('HTTP/1.1 {_status}', ),
+                    tuple(f'{k}: {v}' for k, v in response_dict.items() if not k.startswith('_')),
+                    ('\n', ),
+                )).encode('utf8'))
+                # Send body (if required)
+                if request_dict['method'] != 'HEAD':
+                    connection.sendall(response_dict['_body'])
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
 
     def example_func_dispatch(request_dict, response_dict):
+        print(request_dict)
         response_dict['_body'] = b'Text is good. Dont ask me about ' + request_dict['path'].encode('utf8')
         return response_dict
 
