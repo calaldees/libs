bf100bc855bcc18607bf15bb55e2b1f9350129fe
Allan Callaghan <calaldees@gmail.com>
2017-12-08 08:15:37 +0000

    Added stub for pyramid cache_manager

---
+++ python3/lib/pyramid_helpers/cache_manager.py
@@ -0,0 +1,78 @@
+from collections import namedtuple
+from itertools import chain
+from functools import partial
+
+
+CacheFunctionWrapper = namedtuple('CacheFunctionWrapper', ('func', 'named_positional_args'))
+
+
+class CacheBucket():
+
+    @staticmethod
+    def join_args_func(*args):
+        return '-'.join(map(str(args)))
+
+    def __init__(self, bucket, version=None):
+        self.version = int(version or random.randint(0, 2000000000))
+        self.bucket = bucket
+        self._func_store = {
+            'invalidate_callbacks': [],
+            'cache_key_generaters': []
+        }
+        self.register_cache_key_generator(self.join_args_func, ('bucket', 'version'))
+
+
+    def _register_functionwrapper(self, _type, func, named_positional_args=()):
+        self._func_store[_type].append(
+            self.CacheFunctionWrapper(func, tuple(named_positional_args))
+        )
+
+    def _set_default_keys(self, kwargs):
+        kwargs.setdefault('bucket', self.bucket)
+        kwargs.setdefault('version', self.version)
+
+    def _apply_funcs(self, _type, kwargs):
+        self._set_default_keys(kwargs)
+        required_arguments = set(chain.from_iterable(function_wrapper.named_positional_args for function_wrapper in self._func_store[_type]))
+        assert required_arguments >= kwargs.keys(), f'CacheManager function executed with kwargs {kwargs.keys()} but required to pass {required_arguments}'
+        return (
+            func(*(kwargs.get(attr) for attr in named_positional_args))
+            for func, named_positional_args in self._func_store[_type]
+        )
+
+    def register_invalidate_callback(self, func, named_positional_args=()):
+        self._register_functionwrapper('invalidate_callbacks', func, named_positional_args)
+
+    def register_cache_key_generator(self, func, named_positional_args=()):
+        self._register_functionwrapper('cache_key_generaters', func, named_positional_args)
+
+    def invalidate(self, **kwargs):
+        self._apply_funcs('invalidate_callbacks', kwargs)
+        self.version += 1
+
+    def cache_key(self, **kwargs):
+        return self.join_args_func(*self._apply_funcs('cache_key_generaters', kwargs))
+
+
+class CacheManager():
+
+    def __init__(self, cache_store, default_cache_key_generators=()):  #commit_func,
+        #self.commit_func = commit_func
+        self._cache_store = cache_store
+        self._cache_buckets = {}
+        self._default_cache_key_generators = list(default_cache_key_generators)
+
+    def _create_item(self, bucket):
+        cache_bucket = CacheBucket(bucket=bucket)
+        #cm.register_invalidate_callback(self.commit_func)
+        cache_bucket.register_invalidate_callback(self.cache_store.delete, ('bucket', ))
+        for cache_key_generator in self._default_cache_key_generators:
+            cache_bucket.register_cache_key_generator(*cache_key_generator)
+        cache_bucket.get_or_create = partial(self.cache.get_or_create, bucket)
+        return cache_bucket
+
+    def get(self, bucket):
+        if bucket not in self._cache_buckets:
+            self._cache_buckets[bucket] = self._create_buckets(bucket)
+        return self._cache_buckets[bucket]
+
