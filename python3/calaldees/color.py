8462e2ca95425314d6ced58dc73142976600a1c0
Allan Callaghan <calaldees@gmail.com>
2022-02-06 21:01:59 +0000

    Added python3 color rotator WAY more complex than I first anticipated

--- python3/calaldees/color.py
+++ python3/calaldees/color.py
@@ -69,3 +69,45 @@ def color_distance(target, actual, threshold=20):
 def color_close(target, actual, threshold=20):
     distance = color_distance(target, actual, threshold)
     return True if distance is not None else False
+
+
+from math import sqrt,cos,sin,radians
+class RGBRotate(object):
+    """
+    https://stackoverflow.com/a/8510751/3356840
+    Matrix Operations for Image Processing - Paul Haeberli Nov 1993
+    http://www.graficaobscura.com/matrix/index.html
+    >>> RGBRotate(0).apply(255,0,0)
+    (255, 0, 0)
+    >>> RGBRotate(360).apply(255,0,0)
+    (255, 0, 0)
+    >>> RGBRotate(120).apply(255,0,0)
+    (0, 255, 0)
+    """
+    @staticmethod
+    def clamp(v):
+        if v < 0:
+            return 0
+        if v > 255:
+            return 255
+        return int(v + 0.5)
+    def __init__(self, degrees):
+        self.matrix = [[1,0,0],[0,1,0],[0,0,1]]
+        self.set_hue_rotation(degrees)
+    def set_hue_rotation(self, degrees):
+        cosA = cos(radians(degrees))
+        sinA = sin(radians(degrees))
+        self.matrix[0][0] = cosA + (1.0 - cosA) / 3.0
+        self.matrix[0][1] = 1./3. * (1.0 - cosA) - sqrt(1./3.) * sinA
+        self.matrix[0][2] = 1./3. * (1.0 - cosA) + sqrt(1./3.) * sinA
+        self.matrix[1][0] = 1./3. * (1.0 - cosA) + sqrt(1./3.) * sinA
+        self.matrix[1][1] = cosA + 1./3.*(1.0 - cosA)
+        self.matrix[1][2] = 1./3. * (1.0 - cosA) - sqrt(1./3.) * sinA
+        self.matrix[2][0] = 1./3. * (1.0 - cosA) - sqrt(1./3.) * sinA
+        self.matrix[2][1] = 1./3. * (1.0 - cosA) + sqrt(1./3.) * sinA
+        self.matrix[2][2] = cosA + 1./3. * (1.0 - cosA)
+    def apply(self, r, g, b):
+        rx = r * self.matrix[0][0] + g * self.matrix[0][1] + b * self.matrix[0][2]
+        gx = r * self.matrix[1][0] + g * self.matrix[1][1] + b * self.matrix[1][2]
+        bx = r * self.matrix[2][0] + g * self.matrix[2][1] + b * self.matrix[2][2]
+        return self.clamp(rx), self.clamp(gx), self.clamp(bx)
