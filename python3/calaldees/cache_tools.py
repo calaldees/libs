c941696b6960bf697017c49851dae64552059400
Allan Callaghan <calaldees@gmail.com>
2022-07-29 15:53:04 +0100

    Updated cache_disk to be a decorator

--- python3/calaldees/cache_tools.py
+++ python3/calaldees/cache_tools.py
@@ -2,25 +2,37 @@ import datetime
 from pathlib import Path
 import pickle
 import hashlib
+from functools import wraps
+
+import logging
+log = logging.getLogger(__name__)
 
 
-class CacheProxy():
-    def __init__(self, acquire_data_function, path=Path('_cache'), cache_only=False, ttl=datetime.timedelta(days=1)):
-        assert callable(acquire_data_function)
-        self.acquire_data_function = acquire_data_function
-        self.path = path
-        self.path.mkdir(exist_ok=True)
-        self.cache_only = cache_only
-        assert ttl.seconds
-        self.ttl = ttl
-    def acquire(self, *args, **kwargs):
-        cache = self.path.joinpath(hashlib.sha1(pickle.dumps((args, kwargs))).hexdigest())
-        if cache.is_file() and (datetime.datetime.fromtimestamp(cache.stat().st_mtime) > datetime.datetime.now() - self.ttl):
-            with cache.open(mode='rb') as f:
-                return f.read()
-        if self.cache_only:
-            return
-        data = self.acquire_data_function(*args, **kwargs)
-        with cache.open(mode='wb') as f:
-            f.write(data)
-        return data
+def cache_disk(original_function=None, cache_path=Path('__cache'), ttl=datetime.timedelta(days=1), cache_only=False, args_to_bytes_func=lambda *args, **kwargs: pickle.dumps((args, kwargs))):
+    """
+    """
+    assert isinstance(cache_path, Path)
+    assert isinstance(ttl, datetime.timedelta)
+    cache_path.mkdir(exist_ok=True)
+
+    def _decorate(function):
+        @wraps(function)
+        def wrapped_function(*args, **kwargs):
+            cache = cache_path.joinpath(hashlib.sha1(args_to_bytes_func(*args, **kwargs)).hexdigest())
+            if cache.is_file() and (
+                datetime.datetime.fromtimestamp(cache.stat().st_mtime) > datetime.datetime.now() - ttl
+            ):
+                log.info(f'loading from cache {args=} {kwargs=}')
+                return pickle.load(cache.open(mode='rb'))
+            if cache_only:
+                log.debug(f'cache_only - refusing to run original function')
+                return
+
+            _return = function(*args, **kwargs)  # Wrapped function
+
+            log.info(f'persisting to cache {args=} {kwargs=}')
+            pickle.dump(_return, cache.open(mode='wb'))
+            return _return
+        return wrapped_function
+    return _decorate(original_function) if callable(original_function) else _decorate
+
