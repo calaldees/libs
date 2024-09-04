90cfcd41b4c5a29d5a0d1bca6d0c07e0d4898ff2
Allan Callaghan <allan.callaghan@global.com>
2024-09-04 15:03:31 +0100

    Few thoughts on python iterator chaining

--- python3/calaldees/iterator.py
+++ python3/calaldees/iterator.py
@@ -1,5 +1,6 @@
-from itertools import chain, zip_longest
+from itertools import chain, zip_longest, filterfalse
 from functools import partial, reduce
+
 
 def flatten(iterable):
     """
@@ -19,6 +20,25 @@ def grouper(n, iterable, fillvalue=None):
     """
     return zip_longest(*((iter(iterable),) * n), fillvalue=fillvalue)
 
+
+# https://docs.python.org/3/library/itertools.html#itertools-recipes
+def unique_everseen(iterable, key=None):
+    "Yield unique elements, preserving order. Remember all elements ever seen."
+    # unique_everseen('AAAABBBCCDAABBB') → A B C D
+    # unique_everseen('ABBcCAD', str.casefold) → A B c D
+    seen = set()
+    if key is None:
+        for element in filterfalse(seen.__contains__, iterable):
+            seen.add(element)
+            yield element
+    else:
+        for element in iterable:
+            k = key(element)
+            if k not in seen:
+                seen.add(k)
+                yield element
+
+
 class IteratorCombine():
     r"""
     >>> i = IteratorCombine().map(lambda x: x+1).filter(lambda y: y>3)
@@ -30,10 +50,20 @@ class IteratorCombine():
     >>> i = i.flatten()
     >>> tuple(i.process((1,2,3,4,5)))
     (4, 5, 6, None)
+    >>> i = i.unique()
+    >>> tuple(i.process((1,2,3,4,4,5,5,1)))
+    (4, 5, 6, None)
 
     >>> do_thing = IteratorCombine().map(partial(int.to_bytes, length=2, byteorder='big')).flatten().func(bytes).process
     >>> do_thing(b'abc')
     b'\x00a\x00b\x00c'
+
+    # >>> def reducer(acc, item):
+    # ...     acc += item
+    # ...     return acc
+    # >>> i = IteratorCombine().reduce(reducer)
+    # >>> tuple(i.process((1,2,3,4,5)))
+
     """
     def __init__(self, operations=None):
         self._operations = operations or tuple()
@@ -47,8 +77,12 @@ class IteratorCombine():
         return self.func(partial(grouper, n))
     def flatten(self):
         return self.func(flatten)
+    def unique(self):
+        return self.func(unique_everseen)
     def reduction(self):
         raise NotImplementedError()
         return self.func()  # TODO: similar to `group`. It process multiple items and returns a single item.
+    #def reduce(self, reducer, initializer=None):
+    #    return self.func(partial(reduce, reducer, initializer=initializer))
     def process(self, data):
         return reduce(lambda iterable, func: func(iterable), self._operations, data)
